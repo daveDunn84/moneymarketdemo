b05ee7ce40bcbb1edb413d590e005ea8a5598cf3
Dave Dunn <63639593+daveDunn84@users.noreply.github.com>
2024-03-15 10:59:29 +0200

    Added generateReport endpoint and logging capabilities to the Report Controller

--- src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/presentation/controllers/ReportController.kt
+++ src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/presentation/controllers/ReportController.kt
@@ -1,9 +1,26 @@
 package com.moneymarket.shoprite.moneymarketdemo.presentation.controllers
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.moneymarket.shoprite.moneymarketdemo.domainservices.services.ReportService
+import com.moneymarket.shoprite.moneymarketdemo.presentation.models.DepositRequest
+import com.moneymarket.shoprite.moneymarketdemo.presentation.models.ReportResponse
+import com.moneymarket.shoprite.moneymarketdemo.presentation.models.TransactionResponse
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("report")
-class ReportController {
+@RequestMapping("/report")
+class ReportController(val reportService: ReportService) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    @GetMapping("/generate")
+    fun GenerateReport(@RequestParam(required = true) accountNumber: String): ResponseEntity<ReportResponse> {
+        try {
+            val data = reportService.GenerateReportTest(accountNumber)
+            return ResponseEntity(ReportResponse(null), HttpStatus.OK)
+        } catch (e: Exception) {
+            // todo: send a meaningful error from the report service
+            return ResponseEntity(ReportResponse(null), HttpStatus.BAD_REQUEST)
+        }
+    }
 }
