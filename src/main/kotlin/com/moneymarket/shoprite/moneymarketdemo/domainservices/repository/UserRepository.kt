25c35c3bc4ecb486dd28304bb1d76ad67aa58dbf
Dave Dunn <63639593+daveDunn84@users.noreply.github.com>
2024-03-15 10:54:56 +0200

    Added SaveUser method to the UserRepository interface

--- src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/domainservices/repository/UserRepository.kt
+++ src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/domainservices/repository/UserRepository.kt
@@ -3,5 +3,6 @@ package com.moneymarket.shoprite.moneymarketdemo.domainservices.repository
 import org.apache.catalina.User
 
 interface UserRepository {
-    fun GetUser(username: String): User?
+    fun SaveUser(user: com.moneymarket.shoprite.moneymarketdemo.domainentities.models.User): com.moneymarket.shoprite.moneymarketdemo.domainentities.models.User
+    fun GetUser(accountId: String) : com.moneymarket.shoprite.moneymarketdemo.domainentities.models.User
 }
