3b5f112d0402f5ea2462a34bfe4a172ef1b15758
Dave Dunn <63639593+daveDunn84@users.noreply.github.com>
2024-03-15 10:54:02 +0200

    Added necessary logic to the User class to make it a Database Entity

--- src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/domainentities/models/User.kt
+++ src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/domainentities/models/User.kt
@@ -1,12 +1,38 @@
 package com.moneymarket.shoprite.moneymarketdemo.domainentities.models
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import java.util.Date
 import java.util.UUID
 
+@Entity
+@Table(name = "user", schema="shoprite")
 data class User (
-    private val id: UUID,
-    private val firstname: String,
-    private val lastname: String,
-    private val username: String,
-    private val passwordHash: String,
-    private val passwordSalt: String
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(name = "accountnumber", nullable = false)
+    var accountNumber: String = "",
+
+    @Column(name = "firstname", nullable = false)
+    var firstname: String = "",
+
+    @Column(name = "lastname", nullable = false)
+    var lastname: String = "",
+
+    @Column(name = "username", nullable = false)
+    val username: String = "",
+
+    @Column(name = "passwordhash", nullable = true)
+    var passwordHash: String? = null,
+
+    @Column(name = "passwordsalt", nullable = true)
+    var passwordSalt: String? = null,
+
+    @CreationTimestamp
+    val createdAt: Date? = null
 )
