6db93f5d8f163a3e8bb9bf2ed937f57d2dd5960e
Dave Dunn <63639593+daveDunn84@users.noreply.github.com>
2024-03-15 10:55:24 +0200

    Added necessary logic to the Transaction class to make it a Database Entity

--- src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/domainentities/models/Transaction.kt
+++ src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/domainentities/models/Transaction.kt
@@ -1,13 +1,31 @@
 package com.moneymarket.shoprite.moneymarketdemo.domainentities.models
 
 import com.moneymarket.shoprite.moneymarketdemo.domainentities.enums.TransactionType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import java.math.BigDecimal
 import java.util.UUID
 
+@Entity
+@Table(name = "transaction", schema = "shoprite")
 data class Transaction (
-    private val id: UUID,
-    private val userId: UUID,
-    private val transactionType: TransactionType,
-    private val currency: String,
-    private val amount: BigDecimal
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    @JoinColumn
+    @ManyToOne
+    val user: User,
+
+    @Column(name = "transactiontype", nullable = false)
+    val transactionType: TransactionType,
+
+    @Column(name = "currency", nullable = false)
+    val currency: String,
+
+    @Column(name = "amount", nullable = false)
+    val amount: BigDecimal
 )
