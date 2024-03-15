f708c1973cacb949c50362e94e67012d96e723dd
Dave Dunn <63639593+daveDunn84@users.noreply.github.com>
2024-03-15 11:04:31 +0200

    Added logging capabilities to the AuthServiceImplementation

--- src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/applicationservices/AuthServiceImplementation.kt
+++ src/main/kotlin/com/moneymarket/shoprite/moneymarketdemo/applicationservices/AuthServiceImplementation.kt
@@ -1,13 +1,19 @@
 package com.moneymarket.shoprite.moneymarketdemo.applicationservices
 
 import com.moneymarket.shoprite.moneymarketdemo.domainentities.models.AuthToken
+import com.moneymarket.shoprite.moneymarketdemo.domainentities.models.User
+import com.moneymarket.shoprite.moneymarketdemo.domainservices.repository.UserRepository
 import com.moneymarket.shoprite.moneymarketdemo.domainservices.services.AuthService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class AuthServiceImplementation : AuthService {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun AuthenticateUser(username: String, password: String): AuthToken? {
-        // todo: token generation logic
+        logger.warn("Returning hard coded auth token for testing purposes.")
         return AuthToken("ABCD1234")
     }
 
